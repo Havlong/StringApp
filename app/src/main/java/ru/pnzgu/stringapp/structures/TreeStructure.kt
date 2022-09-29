cf42622dc85a8cd651ecd257291fcaaf7aa3a0c3
Havlong <grishin_evgenii@inbox.ru>
2022-09-29 20:09:55 +0300

    Revert to previous version of DigitalTree

--- app/src/main/java/ru/pnzgu/stringapp/structures/TreeStructure.kt
+++ app/src/main/java/ru/pnzgu/stringapp/structures/TreeStructure.kt
@@ -1,26 +1,90 @@
 package ru.pnzgu.stringapp.structures
 
-import DigitalTree
 import ru.pnzgu.stringapp.DataStructure
 
 class TreeStructure(private val onCompare: () -> Unit) : DataStructure {
-    private val tree = DigitalTree()
+    private val tree = mutableListOf<MutableList<Pair<Char, Int>>>(mutableListOf())
 
     override fun clear() {
         tree.clear()
+        tree.add(mutableListOf())
     }
 
     override fun add(word: String): Boolean {
-        if (word in tree) return false
-        tree += word
-        return true
+        var pos = 0
+        var prevPos: Int
+        for (i in 0 until word.length - 1) {
+            val c = word[i]
+            prevPos = pos
+            for ((char, nextPos) in tree[pos]) {
+                onCompare()
+                if (char == c && nextPos != -1) {
+                    pos = nextPos
+                    break
+                }
+            }
+            onCompare()
+            if (pos == prevPos) {
+                pos = tree.size
+                tree.add(mutableListOf())
+                tree[prevPos].add(c to pos)
+            }
+        }
+        onCompare()
+        return if (word.last() to -1 !in tree[pos]) {
+            tree[pos].add(word.last() to -1)
+            true
+        } else {
+            false
+        }
     }
 
-    override fun contains(word: String): Boolean = word in tree
+    override fun contains(word: String): Boolean {
+        var pos = 0
+        var prevPos: Int
+        for (i in 0 until word.length - 1) {
+            val c = word[i]
+            prevPos = pos
+            for ((char, nextPos) in tree[pos]) {
+                onCompare()
+                if (char == c && nextPos != -1) {
+                    pos = nextPos
+                    break
+                }
+            }
+            onCompare()
+            if (pos == prevPos) {
+                return false
+            }
+        }
+        onCompare()
+        return word.last() to -1 in tree[pos]
+    }
 
     override fun remove(word: String): Boolean {
-        if (word !in tree) return false
-        tree -= word
-        return true
+        var pos = 0
+        var prevPos: Int
+        for (i in 0 until word.length - 1) {
+            val c = word[i]
+            prevPos = pos
+            for ((char, nextPos) in tree[pos]) {
+                onCompare()
+                if (char == c && nextPos != -1) {
+                    pos = nextPos
+                    break
+                }
+            }
+            onCompare()
+            if (pos == prevPos) {
+                return false
+            }
+        }
+        onCompare()
+        return if (tree[pos].contains(word.last() to -1)) {
+            tree[pos].remove(word.last() to -1)
+            true
+        } else {
+            false
+        }
     }
 }
